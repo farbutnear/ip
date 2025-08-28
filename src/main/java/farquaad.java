83c8d32b179e0a7a822036bb32535a93984ff66f
Asher <gohjinyuan@gmail.com>
2025-08-28 21:59:18 +0800

    Level-0: Greet

---
+++ src/main/java/farquaad.java
@@ -0,0 +1,7 @@
+public class farquaad {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Farquaad");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
