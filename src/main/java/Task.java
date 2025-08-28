b5ef2083038fb1b07c2e1399adffd958a2b231d8
Asher <gohjinyuan@gmail.com>
2025-08-29 04:35:39 +0800

    Level-4: ToDos, Events, Deadlines

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -24,4 +24,40 @@ public class Task {
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
     }
+
+    static class ToDo extends Task {
+        public ToDo(String description) {
+            super(description);
+        }
+        @Override
+        public String toString() {
+            return "[T]" + super.toString();
+        }
+    }
+
+    static class Deadline extends Task {
+        private String day;
+        public Deadline(String description, String day) {
+            super(description);
+            this.day = day;
+        }
+        @Override
+        public String toString() {
+            return "[D]" + super.toString() + " (by: " + day + ")";
+        }
+    }
+
+    static class Event extends Task {
+        private String start;
+        private String end;
+        public Event(String description, String start, String end) {
+            super(description);
+            this.start = start;
+            this.end = end;
+        }
+        @Override
+        public String toString() {
+            return "[E]" + super.toString() + " (from: " + start + "to: " + end + ")";
+        }
+    }
 }
--- src/main/java/farquaad.java
+++ src/main/java/farquaad.java
@@ -11,6 +11,7 @@ public class farquaad {
         Scanner sc = new Scanner(System.in);
         while (true) {
             String input = sc.nextLine();
+
             if (input.equals("list")) {
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println((i + 1) + ". " + tasks.get(i));
@@ -29,9 +30,29 @@ public class farquaad {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else {
-                Task task = new Task(input);
-                tasks.add(task);
-                System.out.println("added: " + input);
+                System.out.println("Got it. I've added this task: ");
+
+                if (input.startsWith("todo")) {
+                    Task toDo = new Task.ToDo(input.substring(5));
+
+                    tasks.add(toDo);
+                    System.out.println(" " + toDo.toString());
+                } else if (input.startsWith("deadline")) {
+                    String[] splits = input.substring(9).split(" /by ");
+                    Task deadline = new Task.Deadline(splits[0], splits[1]);
+
+                    tasks.add(deadline);
+                    System.out.println("  " + deadline.toString());
+                } else if (input.startsWith("event")) {
+                    String[] firstSplit = input.substring(6).split(" /from ");
+                    String[] secondSplit = firstSplit[1].split("/to ");
+                    Task event = new Task.Event(firstSplit[0], secondSplit[0], secondSplit[1]);
+
+                    tasks.add(event);
+                    System.out.println("  " + event.toString());
+                }
+
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
             }
         }
     }
