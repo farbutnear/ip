3c5f7efee964eaa9aeb2624c799a95da083b682f
Asher <gohjinyuan@gmail.com>
2025-08-28 22:40:52 +0800

    Level-1: Echo

--- src/main/java/farquaad.java
+++ src/main/java/farquaad.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class farquaad {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Farquaad");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(input);
+        }
     }
 }
