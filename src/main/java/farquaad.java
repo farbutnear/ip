0e8386e96dcfebfeb4121f66a582832c3d58fd22
Asher <gohjinyuan@gmail.com>
2025-08-28 22:53:13 +0800

    Level-2: Add, List

--- src/main/java/farquaad.java
+++ src/main/java/farquaad.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class farquaad {
+    static ArrayList<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Farquaad");
         System.out.println("What can I do for you?");
@@ -8,9 +11,16 @@ public class farquaad {
         Scanner sc = new Scanner(System.in);
         while (true) {
             String input = sc.nextLine();
-            if (input.equalsIgnoreCase("bye")) {
+            if (input.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+            } else if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else {
+                list.add(input);
+                System.out.println("added: " + input);
             }
             System.out.println(input);
         }
